a0627e564815c11d1586984f5d92863f7ce69374
eg5or <eg5or@mail.ru>
2021-01-13 23:49:25 +0300

    Header completed menu logo login button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './../../main.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {Icon} from "semantic-ui-react";
+import {NavLink} from "react-router-dom";
 
 const Header = (props) => {
     return <div className='headerContainer'>
@@ -14,21 +13,21 @@ const Header = (props) => {
         <div className='centerHeader'>
             <div className='leftMainMenu'>
                 <ul>
-                    <li>Главная</li>
-                    <li>О нас</li>
-                    <li>Галерея</li>
+                    <li><NavLink to={'/'}>Главная</NavLink></li>
+                    <li><NavLink to={'/about'}>О нас</NavLink></li>
+                    <li><NavLink to={'/portfolio'}>Портфолио</NavLink></li>
                 </ul>
             </div>
             <img className='logoHeader' src='/assets/images/bdlogo.png' />
             <div className='rightMainMenu'>
                 <ul>
-                    <li>Главная</li>
-                    <li>О нас</li>
-                    <li>Галерея</li>
+                    <li><NavLink to={'/price'}>Стоимость</NavLink></li>
+                    <li><NavLink to={'/certificate'}>Сертификаты</NavLink></li>
+                    <li><NavLink to={'/reviews'}>Отзывы</NavLink></li>
                 </ul>
             </div>
         </div>
-        <div className='bottomHeader'>Нижний блок</div>
+        <div className='bottomHeader'/>
     </div>
 }
 
